ae1a6bcbb5882bea723df3e78a1419024d55e95d
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-10-31 17:28:58 -0300

    feat(banner): add required

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -6,6 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent {
-  @Input() src = '';
-  @Input() alt = '';
+  @Input({ required: true }) src = '';
+  @Input({ required: true }) alt = '';
 }
