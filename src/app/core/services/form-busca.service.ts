5d8727f2542e8f69f62877c6343b7b578810768c
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-11-05 12:53:50 -0300

    fix: controlando o modal

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatChipSelectionChange } from '@angular/material/chips';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 /**
  * Serviço que controla o formulário para ser injetado em todos os componentes onde há necessidade de manipulá-lo.
@@ -11,12 +14,41 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class FormBuscaService {
   formBusca: FormGroup;
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.formBusca = new FormGroup({
       somenteIda: new FormControl(false),
       origem: new FormControl(),
       destino: new FormControl(),
+      tipo: new FormControl('Econômica'),
+      adultos: new FormControl(1),
+      criancas: new FormControl(0),
+      bebes: new FormControl(0),
     });
+  }
+
+  getDescricaoPassageiros(): string {
+    let descricao = '';
+
+    const adultos = this.formBusca.get('adultos')?.value;
+    if (adultos && adultos > 0) {
+      descricao += `${adultos} adulto${adultos > 1 ? 's' : ''}`;
+    }
+
+    const criancas = this.formBusca.get('criancas')?.value;
+    if (criancas && criancas > 0) {
+      descricao += `${descricao ? ', ' : ''}${criancas} criança${
+        criancas > 1 ? 's' : ''
+      }`;
+    }
+
+    const bebes = this.formBusca.get('bebes')?.value;
+    if (bebes && bebes > 0) {
+      descricao += `${descricao ? ', ' : ''}${bebes} bebê${
+        bebes > 1 ? 's' : ''
+      }`;
+    }
+
+    return descricao;
   }
 
   obterControle(nome: string): FormControl {
@@ -25,5 +57,21 @@ export class FormBuscaService {
       throw new Error(`FormControl com nome "${nome}" não existe.`);
     }
     return control as FormControl;
+  }
+
+  alterarTipo(event: MatChipSelectionChange, tipo: string) {
+    if (event.selected) {
+      this.formBusca.patchValue({
+        tipo,
+      });
+    }
+    console.log(
+      'Tipo de passagem alterado para: ',
+      this.formBusca.get('tipo')?.value
+    );
+  }
+
+  openDialog() {
+    this.dialog.open(ModalComponent);
   }
 }
